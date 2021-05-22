36b6604f72be3f8bf964453aad55677d9c3497d4
DN-Vanguard <Domiltannguyen@outlook.com>
2021-05-22 03:41:36 -0700

    command prompts for special, numeric, lowercase and uppercase were added.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,13 @@
 // Global variables as individual strings
-var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var numberChar = "0123456789";
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = lowercaseChar.toUpperCase();
+var specialChars = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+var numberChars = "0123456789";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = lowercaseChars.toUpperCase();
 
-var combinedChars = [specialChar,numberChar,lowercaseChar,uppercaseChar];
+var combinedChars = [specialChars,numberChars,lowercaseChars,uppercaseChars];
+var criteriaNames = ["SPECIAL", "NUMBER", "LOWERCASE", "UPPERCASE"]
 
-
+// ////
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -22,17 +23,35 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// /////
 
+//First prompt asking to enter a valid value.
 function askPasswordLength() {
   var length = prompt("Please enter a value between 8 and 128.");
   return length;
 }
+//Second, third, forth, and fifth prompt asking if user wants to include:
+//special, number, lowercase, and/or uppercase to generator.
+function promptCriterias(numberOfCriterias) {
+  var userCriteriaChoices = [];
+  for ( var i=0; i <numberOfCriterias; i++){
+    userCriteriaChoices = confirm("OK to include " + criteriaNames[i] + " characters." 
+    + "\r\nCANCEL to exclude.")
+  }
+  return usersCriteriaChoices;
+}
+
+
+
+
 function generatePassword() {
 
   var passwordLength = askPasswordLength();
     if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("The value entered was invalid. Please try again.");
-    return askPasswordLength();
+    return generatePassword();
     }
+
+  promptCriterias(criteriaNames.length); 
 }
 
