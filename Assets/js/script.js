5fe0bde5dc656112beca0eef16ee6e6ad5324cf4
DN-Vanguard <Domiltannguyen@outlook.com>
2021-05-22 02:49:11 -0700

    First prompt of asking for values is functional. Will display error if vaue is not within parameters.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,12 @@
-// Global variables
-var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+// Global variables as individual strings
+var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var numberChar = "0123456789";
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChar = lowercaseChar.toUpperCase();
+
+var combinedChars = [specialChar,numberChar,lowercaseChar,uppercaseChar];
+
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -13,10 +17,22 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+function askPasswordLength() {
+  var length = prompt("Please enter a value between 8 and 128.");
+  return length;
+}
+function generatePassword() {
+
+  var passwordLength = askPasswordLength();
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("The value entered was invalid. Please try again.");
+    return askPasswordLength();
+    }
+}
 
