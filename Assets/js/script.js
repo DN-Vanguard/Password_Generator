76e404f77b2fffb3ced61a9b1534f60b7d90a7e8
DN-Vanguard <Domiltannguyen@outlook.com>
2021-05-22 17:59:01 -0700

    prompts are now functional and error will appear if user doesnot pick a criteria.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,15 @@
-// Global variables as individual strings
+// Global variables
 var specialChars = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+var splitSpecial = specialChars.split("");
+
 var numberChars = "0123456789";
+var splitNumber = numberChars.split("");
+
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var splitLowercase = lowercaseChars.split("");
+
 var uppercaseChars = lowercaseChars.toUpperCase();
-
-var combinedChars = [specialChars,numberChars,lowercaseChars,uppercaseChars];
-var criteriaNames = ["SPECIAL", "NUMBER", "LOWERCASE", "UPPERCASE"]
-
-// ////
+var splitUppercase = uppercaseChars.split("");
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -23,22 +25,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// /////
 
 //First prompt asking to enter a valid value.
 function askPasswordLength() {
-  var length = prompt("Please enter a value between 8 and 128.");
+  var length = Math.floor(prompt("Please enter a value between 8 and 128."));
   return length;
 }
-//Second, third, forth, and fifth prompt asking if user wants to include:
-//special, number, lowercase, and/or uppercase to generator.
-function promptCriterias(numberOfCriterias) {
-  var userCriteriaChoices = [];
-  for ( var i=0; i <numberOfCriterias; i++){
-    userCriteriaChoices = confirm("OK to include " + criteriaNames[i] + " characters." 
-    + "\r\nCANCEL to exclude.")
-  }
-  return usersCriteriaChoices;
+//Following prompts asking for clarification on which criteria user would like to use.
+//User has to pick atleast one criteria otherwise error would appear.
+function promptCriterias(){
+  var wantSpecialChars = confirm("Would you like SPECIAL characters in your password?\n\nCANCEL for NO || OK for YES")
+  var wantNumberChars = confirm("Would you like NUMBER characters in your password?\n\nCANCEL for NO || OK for YES")
+  var wantLowercaseChars = confirm("Would you like LOWERCASE characters in your password?\n\nCANCEL for NO || OK for YES")
+  var wantUppercaseChars = confirm("Would you like UPPERCASE characters in your password?\n\nCANCEL for NO || OK for YES")
+    if (!wantSpecialChars && !wantNumberChars && !wantLowercaseChars && !wantUppercaseChars) {
+      alert("Please select 1 of the given criterias.");
+    }
+  return "";
 }
 
 
@@ -48,10 +51,15 @@ function generatePassword() {
 
   var passwordLength = askPasswordLength();
     if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    alert("The value entered was invalid. Please try again.");
-    return generatePassword();
+      alert("The value entered was invalid. Please try again.");
+      return "";
     }
 
-  promptCriterias(criteriaNames.length); 
-}
+  promptCriterias();
 
+
+    
+
+    
+
+}
